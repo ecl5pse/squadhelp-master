ebd5967e4703e6994884d3c9763c008aa6c043cb
ecl5pse <fantamas20@gmail.com>
2020-03-31 19:07:04 +0300

    fix sign_up page

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -50,7 +50,7 @@ class Header extends React.Component {
             return (
                 <>
                     <Link to='/login' style={{textDecoration: 'none'}}><span className={styles.btn}>LOGIN</span></Link>
-                    <Link to='/registration' style={{textDecoration: 'none'}}><span
+                    <Link to='/registration' style={{textDecoration: 'none'}}  target='_blank'><span
                         className={styles.btn}>SIGN UP</span></Link>
                 </>
             )
@@ -65,13 +65,12 @@ class Header extends React.Component {
             <div className={styles.headerContainer}>
                 <div className={styles.fixedHeader}>
                     <span className={styles.info}>Squadhelp recognized as one of the Most Innovative Companies by Inc Magazine.</span>
-                    <a href="http://www.google.com">Read Announcement</a>
+                    <a  target='_blank' href="http://www.google.com">Read Announcement</a>
                 </div>
                 <div className={styles.loginSignnUpHeaders}>
                     <div className={styles.numberContainer}>
                         <img src={`${CONSTANTS.STATIC_IMAGES_PATH}phone.png`}
                              alt='phone'/>
-
                         <span>(877)&nbsp;355-3585</span>
                     </div>
                     <div className={styles.userButtonsContainer}>
